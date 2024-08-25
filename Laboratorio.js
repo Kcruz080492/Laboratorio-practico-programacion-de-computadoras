7406ebb253a97362110d3e95a4e94645191a880b
Luiartinez <luismartinez13hernandez@gmail.com>
2024-08-25 16:29:14 -0600

    Add files via upload

---
+++ Laboratorio.js
@@ -0,0 +1,15 @@
+let camisetas = 150
+let pantalones = 20
+let zapatos = 110
+
+let totalproductos = camisetas+pantalones+zapatos
+
+console.log("el total es de productos es "+totalproductos);
+
+if (camisetas < 100 ){console.log("no son necesarias mas camisetas")} 
+
+else if (pantalones < 100){console.log("no se necesitan mas pantalones")} 
+
+else if (zapatos < 100){console.log("no se necesitan mas zapatos")} 
+
+else{console.log("no es necesario reponer ningun producto");} 
